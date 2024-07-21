36604b0a4b1cf834a83c7dad6210f1a9fd038d9b
shajibulhasn <shajibulhasan.work@gmail.com>
2024-07-21 21:05:41 +0600

    middleware added

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,29 @@ use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\totalBidController;
 use App\Http\Controllers\RequestedBidController;
+use App\Http\Middleware\ValidateUser;
+use App\Http\Middleware\ValidateAdmin;
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
 
 
-Route::get('/userDashboard',[UserDashboardController::class, 'index'])->name('userDashboard');
-Route::post('/runningBidUser/{id}', [UserDashboardController::class, 'biddingPrice'])->name('biddingprice.post'); 
-Route::get('/runningBidUser',[UserDashboardController::class, 'runningBid'])->name('runningBidUser');
-Route::get('/myBid',[UserDashboardController::class, 'myBid'])->name('myBid');
-Route::view('/create', 'createBid')->name('create');
-Route::post('/createBid',[UserDashboardController::class, 'requestBid'])->name('createbid.post');
+Route::get('/userDashboard',[UserDashboardController::class, 'index'])->name('userDashboard')->middleware(ValidateUser::class);
+Route::post('/runningBidUser/{id}', [UserDashboardController::class, 'biddingPrice'])->name('biddingprice.post')->middleware(ValidateUser::class); 
+Route::get('/runningBidUser',[UserDashboardController::class, 'runningBid'])->name('runningBidUser')->middleware(ValidateUser::class);
+Route::get('/myBid',[UserDashboardController::class, 'myBid'])->name('myBid')->middleware(ValidateUser::class);
+Route::view('/create', 'createBid')->name('create')->middleware(ValidateUser::class);
+Route::post('/createBid',[UserDashboardController::class, 'requestBid'])->name('createbid.post')->middleware(ValidateUser::class);
 
 
-Route::get('/admin',[AdminController::class, 'index'])->name('admin');
-Route::get('/deleteBid/{id}',[AdminController::class, 'deleteBid'])->name('deleteBid');
-Route::get('/approveBid/{id}',[AdminController::class, 'approveBid'])->name('approveBid');
-Route::get('/totalBid',[AdminController::class, 'totalBid'])->name('totalBid');
-Route::get('/requestedBid',[AdminController::class, 'requestedBid'])->name('requestedBid');
-Route::get('/runningBidAdmin',[UserDashboardController::class, 'runningBid'])->name('runningBidAdmin');
+Route::get('/admin',[AdminController::class, 'index'])->name('admin')->middleware(ValidateAdmin::class);
+Route::get('/deleteBid/{id}',[AdminController::class, 'deleteBid'])->name('deleteBid')->middleware(ValidateAdmin::class);
+Route::get('/approveBid/{id}',[AdminController::class, 'approveBid'])->name('approveBid')->middleware(ValidateAdmin::class);
+Route::get('/totalBid',[AdminController::class, 'totalBid'])->name('totalBid')->middleware(ValidateAdmin::class);
+Route::get('/requestedBid',[AdminController::class, 'requestedBid'])->name('requestedBid')->middleware(ValidateAdmin::class);
+Route::get('/runningBidAdmin',[UserDashboardController::class, 'runningBid'])->name('runningBidAdmin')->middleware(ValidateAdmin::class);
 
 
 
